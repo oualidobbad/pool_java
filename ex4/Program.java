f73854a1f3f8a2b27238a1ceea566b1f2e04bde2
oualid obbad <oualidobbad@gmail.com>
2026-02-08 08:42:04 +0100

    completed

---
+++ ex4/Program.java
@@ -0,0 +1,13 @@
+package ex4;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main (String[]args){
+		int[]arr = new int[65535];
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+
+		scanner.close();
+	}
+}
