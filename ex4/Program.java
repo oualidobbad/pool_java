6340d535660a072935476b5b0a4bb9e634f14b10
Oualid Obbad <oobbad@c2r1p11.1337.ma>
2026-02-08 23:55:29 +0100

    working ex4

--- ex4/Program.java
+++ ex4/Program.java
@@ -3,23 +3,46 @@ package ex4;
 import java.util.Scanner;
 
 public class Program {
+
+	static int max(int[] arr){
+		int max = 0;
+
+		for (int i = 0; i < 65535; i++)
+		{
+			if (arr[i] != 0 && max < arr[i])
+			{
+				max = arr[i];
+				arr[i] = 0;
+			}
+		}
+		return max;
+	}
+
 	public static void main (String[]args){
 		int[]arr = new int[65535];
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
 		char [] caracaters = line.toCharArray();
+		int barr = 10;
+		int maxCount;
+		int max;
 
 		for (char c : caracaters)
-		{
 			arr[c]++;
-		}
-		for (int i = 0; i < 65535; i++)
+		maxCount = max(arr);
+		max = maxCount;
+		for (int i = 0; i < 9; i++)
 		{
-			if (arr[i] != 0)
+			int scaledHeight = (max * 10) / maxCount;
+			for (int j = 10; j > 0; j--)
 			{
-				System.out.print((char)i + " ");
-				System.out.println(arr[i]);
+				if (scaledHeight >= j)
+					System.out.print("#");
+				else
+					System.out.print(" ");
 			}
+			System.out.println();
+			max = max(arr);
 		}
 		scanner.close();
 	}
