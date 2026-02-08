918ad70b5e79e5286348f9e163ad1869ed62dd04
Oualid Obbad <oobbad@c2r3p13.1337.ma>
2026-02-08 15:19:55 +0100

    working ex4

--- ex4/Program.java
+++ ex4/Program.java
@@ -7,7 +7,20 @@ public class Program {
 		int[]arr = new int[65535];
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
+		char [] caracaters = line.toCharArray();
 
+		for (char c : caracaters)
+		{
+			arr[c]++;
+		}
+		for (int i = 0; i < 65535; i++)
+		{
+			if (arr[i] != 0)
+			{
+				System.out.print((char)i + " ");
+				System.out.println(arr[i]);
+			}
+		}
 		scanner.close();
 	}
 }
