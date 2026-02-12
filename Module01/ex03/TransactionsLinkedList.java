ba1997c0e0ba4d19e51bf2d1d84aa5fdc36ecb75
oualid obbad <oualidobbad@gmail.com>
2026-02-12 23:28:26 +0100

    completed

--- Module01/ex03/TransactionsLinkedList.java
+++ Module01/ex03/TransactionsLinkedList.java
@@ -1,36 +1,96 @@
 package Module01.ex03;
+import java.util.UUID;
+
 import Module01.ex00.Transaction;
 
 public class TransactionsLinkedList implements	TransactionsList {
 	
 	private List head;
-	private List last;
+	private List lastNode;
+	private int size;
 
 	private static class List {
 		private Transaction transaction;
 		List next;
+		List prev;
+
 		List(Transaction transaction){
 			this.transaction = transaction;
 			next = null;
+			prev = null;
 		}
 	}
-	TransactionsLinkedList(){
-		next = null;
-		// head = null;
-		// last = null;
+	public TransactionsLinkedList()
+	{
+		head = null;
+		lastNode = null;
+		size = 0;
 	}
 	@Override
 	public void addTransaction(Transaction tr){
-		this.transaction = tr;
-		TransactionsLinkedList node = new TransactionsLinkedList();
-		if (next == null){
-			next = node;
+		if (tr == null)
+			return ;
+		List node = new List(tr);
+		if (head == null){
+			head = node;
+			lastNode = node;
 		}
 		else
 		{
-			next
+			lastNode.next = node;
+			node.prev = lastNode;
+			lastNode = node;
 		}
+		size++;
 	}
 
+	@Override
+	public void deleteTransaction(UUID id){
+		List curreNode;
 
+		if (head == null)
+			return ;
+		size--;
+		if (head == lastNode && id.equals(head.transaction.getIdentifier()))
+		{
+			head = null;
+			lastNode = null;
+			return ;
+		}
+		else if (id.equals(head.transaction.getIdentifier()))
+		{
+			head = head.next;
+			head.prev = null;
+			return ;
+		}
+		else if (id.equals(lastNode.transaction.getIdentifier()))
+		{
+			lastNode = lastNode.prev;
+			lastNode.next = null;
+			return ;
+		}
+		curreNode = head;
+		while (curreNode != null) {
+			if (id.equals(curreNode.transaction.getIdentifier()))
+			{
+				curreNode.prev.next = curreNode.next;
+				curreNode.next.prev = curreNode.prev;
+				return ;
+			}
+			curreNode = curreNode.next;
+		}
+		size++;
+	}
+
+	@Override
+	public Transaction[] toArray(){
+		Transaction [] arr = new Transaction[size];
+		List currList = head;
+		int i = 0;
+		while (currList != null) {
+			arr[i++] = currList.transaction;
+			currList = currList.next;
+		}
+		return arr;
+	}
 }
