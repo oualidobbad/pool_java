11dbbe17c1edbb7fc4c1e5711a70146ea94691b5
oualidobbad <oualidobbad@gmail.com>
2026-02-20 00:12:08 +0000

    working on ex04

--- Module01/ex04/Program.java
+++ Module01/ex04/Program.java
@@ -2,7 +2,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		User usr1 = new User("admin", 1000);
-		User usr2 = new User("khalid", 15);
+		User usr2 = new User("oualid", 15);
 		TransactionsService trs = new TransactionsService();
 		trs.addUser(usr1);
 		trs.addUser(usr2);
--- Module01/ex04/TransactionsService.java
+++ Module01/ex04/TransactionsService.java
@@ -1,44 +1,50 @@
+import java.util.UUID;
+
 public class TransactionsService {
     private UsersList usrList;
 
-    public TransactionsService(){
+    public TransactionsService() {
         usrList = new UsersArrayList();
     }
-    public void addUser(User usr)
-    {
+
+    public void addUser(User usr) {
         usrList.addUser(usr);
     }
-    public Integer getBalanceUser(Integer id){
-        try{
+
+    public Integer getBalanceUser(Integer id) {
+        try {
             return usrList.getUserById(id).getBalance();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return 0;
     }
 
-    public void transferTransaction(Integer senderId, Integer recipientId, Integer amount){
-        try{
-            User sender = usrList.getUserById(senderId);
-            User received = usrList.getUserById(recipientId);
-            Transaction creditTransaction;
-            Transaction debitTransaction;
+    public void transferTransaction(Integer senderId, Integer recipientId, Integer amount) {
+        User sender = usrList.getUserById(senderId);
+        User received = usrList.getUserById(recipientId);
+        Transaction creditTransaction;
+        Transaction debitTransaction;
 
-            if (sender.getBalance() < amount || amount <= 0)
-                throw new IllegalTransactionException("operation Faild!, check amount");
-            received.setBalance(received.getBalance() + amount);
-            sender.setBalance(sender.getBalance() - amount);
-            debitTransaction = new Transaction(sender, received, TransferCategory.DEBIT, -amount);
-            sender.getList().addTransaction(debitTransaction);
-            creditTransaction = new Transaction(sender, received, TransferCategory.CREDIT, amount);
-            creditTransaction.setIdentifier(debitTransaction.getIdentifier());
-            received.getList().addTransaction(creditTransaction);
-            debitTransaction.printInfo();
-            creditTransaction.printInfo();
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
+        if (sender.getBalance() < amount || amount <= 0)
+            throw new IllegalTransactionException("operation Failed!, check amount");
+        received.setBalance(received.getBalance() + amount);
+        sender.setBalance(sender.getBalance() - amount);
+        debitTransaction = new Transaction(sender, received, TransferCategory.DEBIT, -amount);
+        sender.getList().addTransaction(debitTransaction);
+        creditTransaction = new Transaction(sender, received, TransferCategory.CREDIT, amount);
+        creditTransaction.setIdentifier(debitTransaction.getIdentifier());
+        received.getList().addTransaction(creditTransaction);
     }
+
+    public Transaction[] getTransactions(Integer userId){
+
+        User usr = usrList.getUserById(userId);
+        return usr.getList().toArray();
+    }
+    public void removeTransaction(Integer userId, UUID transactionId){
+        User user = usrList.getUserById(userId);
+        user.getList().deleteTransaction(transactionId);
+    }
+
 }
