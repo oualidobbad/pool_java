097b9717fa4fa202dcc9ebccf4deaf753cff8b8a
oualidobbad <oualidobbad@gmail.com>
2026-02-23 17:40:10 +0000

    done

---
+++ Module01/ex05_app/run.sh
@@ -0,0 +1,11 @@
+# Compile
+./mvnw clean compile
+
+# Run directly via Maven
+./mvnw javafx:run
+
+# Package as runnable JAR
+./mvnw clean package
+
+# Run the standalone JAR
+java -jar target/ex05-app-1.0.0.jar
