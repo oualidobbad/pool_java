4a0c6cc9ef96981522710c8ec814eef950f4a4a5
oualid obbad <oualidobbad@gmail.com>
2026-02-24 02:00:29 +0000

    finished Module 01, next ....

--- Module01/ex05_app/src/main/java/com/transactions/app/Launcher.java
+++ Module01/ex05_app/src/main/java/com/transactions/app/Launcher.java
@@ -5,7 +5,10 @@ package com.transactions.app;
  * (JavaFX Application subclass cannot be the main-class of a fat JAR.)
  */
 public class Launcher {
-    public static void main(String[] args) {
-        TransactionsApp.main(args);
-    }
+	public static void main(String[] args) {
+		// Fix garbled / blurry text rendering on Linux (LCD sub-pixel AA bug)
+		System.setProperty("prism.lcdtext", "false");
+		System.setProperty("prism.text", "t2k");
+		TransactionsApp.main(args);
+	}
 }
--- Module01/ex05_app/src/main/java/com/transactions/ui/components/Animations.java
+++ Module01/ex05_app/src/main/java/com/transactions/ui/components/Animations.java
@@ -1,9 +1,9 @@
 package com.transactions.ui.components;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
-import javafx.animation.ParallelTransition;
+import javafx.animation.*;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
 /**
@@ -11,27 +11,51 @@ import javafx.util.Duration;
  */
 public class Animations {
 
-    /** Fade-in + slight slide-up for panel switching. */
-    public static void fadeSlideIn(Node node) {
-        node.setOpacity(0);
-        node.setTranslateY(18);
+	/** Fade-in + slight slide-up for panel switching. */
+	public static void fadeSlideIn(Node node) {
+		node.setOpacity(0);
+		node.setTranslateY(12);
 
-        FadeTransition ft = new FadeTransition(Duration.millis(300), node);
-        ft.setFromValue(0);
-        ft.setToValue(1);
+		FadeTransition ft = new FadeTransition(Duration.millis(250), node);
+		ft.setFromValue(0);
+		ft.setToValue(1);
+		ft.setInterpolator(Interpolator.EASE_OUT);
 
-        TranslateTransition tt = new TranslateTransition(Duration.millis(300), node);
-        tt.setFromY(18);
-        tt.setToY(0);
+		TranslateTransition tt = new TranslateTransition(Duration.millis(250), node);
+		tt.setFromY(12);
+		tt.setToY(0);
+		tt.setInterpolator(Interpolator.EASE_OUT);
 
-        new ParallelTransition(ft, tt).play();
-    }
+		new ParallelTransition(ft, tt).play();
+	}
 
-    /** Quick subtle fade. */
-    public static void fadeIn(Node node) {
-        node.setOpacity(0);
-        FadeTransition ft = new FadeTransition(Duration.millis(200), node);
-        ft.setToValue(1);
-        ft.play();
-    }
+	/** Cross-fade: old fades out while new fades in simultaneously. */
+	public static void crossFade(Node oldNode, Node newNode, StackPane parent) {
+		newNode.setOpacity(0);
+		newNode.setTranslateY(0);
+		StackPane.setAlignment(newNode, Pos.TOP_LEFT);
+		parent.getChildren().add(newNode);
+
+		FadeTransition fadeOut = new FadeTransition(Duration.millis(100), oldNode);
+		fadeOut.setFromValue(1);
+		fadeOut.setToValue(0);
+		fadeOut.setInterpolator(Interpolator.EASE_IN);
+		fadeOut.setOnFinished(e -> parent.getChildren().remove(oldNode));
+
+		FadeTransition fadeIn = new FadeTransition(Duration.millis(100), newNode);
+		fadeIn.setFromValue(0);
+		fadeIn.setToValue(1);
+		fadeIn.setInterpolator(Interpolator.EASE_OUT);
+
+		new ParallelTransition(fadeOut, fadeIn).play();
+	}
+
+	/** Quick subtle fade. */
+	public static void fadeIn(Node node) {
+		node.setOpacity(0);
+		FadeTransition ft = new FadeTransition(Duration.millis(200), node);
+		ft.setToValue(1);
+		ft.setInterpolator(Interpolator.EASE_OUT);
+		ft.play();
+	}
 }
