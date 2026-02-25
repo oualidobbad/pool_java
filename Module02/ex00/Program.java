d1e58186aba4fa6123e4ceee5eecbe0334a7940e
Oualid Obbad <oobbad@c1r5p14.1337.ma>
2026-02-25 16:33:41 +0000

    working 05

--- Module02/ex00/Program.java
+++ Module02/ex00/Program.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
 
 public class Program {
     public static void main(String [] args){
 		ReadSignature signature= new ReadSignature();
-		try
-		{
+
+		try {
 			signature.parseSignature();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
+		}
+		ReadInputUsrFile usrin = new ReadInputUsrFile();
+		Scanner sc = new Scanner(System.in);
+		String line = "";
+		while (true) {
+			try
+			{
+				System.out.print("-> ");
+				line = sc.nextLine().trim();
+				if (line.equals("42"))
+					return ;
+				usrin.typeSignature(line);
+			}
+			catch (Exception e)
+			{
+				System.out.println(e.getMessage());
+			}
 		}
     }
 }
--- Module02/ex00/ReadInputUsrFile.java
+++ Module02/ex00/ReadInputUsrFile.java
@@ -4,8 +4,18 @@ import java.io.InputStream;
 
 public class ReadInputUsrFile {
 
-	public void typeSignature(String fileUsrInput) throws Exception{
+	public void typeSignature(String fileUsrInput) throws Exception {
+
 		InputStream in = new FileInputStream(fileUsrInput);
-		
+
+		byte[] buffer = new byte[8];
+		int bytesRead = in.read(buffer);
+
+		String eightByte = "";
+		for (int i = 0; i < bytesRead; i++) {
+			eightByte += String.format("%02X ", buffer[i]);
+		}
+		System.out.println(eightByte);
 	}
 }
+
