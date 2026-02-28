c08a85045014f1bf9222df6ba3da866c22081743
Oualid Obbad <oobbad@c1r1p12.1337.ma>
2026-02-28 21:33:39 +0000

    working ex02

--- Module02/ex02/Program.java
+++ Module02/ex02/Program.java
@@ -1,18 +1,37 @@
+
+import java.io.File;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Program {
 
+    static Path changeDir(Path current, String arg) {
+
+        Path target = current.resolve(arg).normalize();
+		File file = new File(target.toString());
+
+        if (!file.exists()) {
+            System.out.println("no such file or directory: " + arg);
+            return current;
+        }
+        if (!file.isDirectory()) {
+            System.out.println("not a directory: " + arg);
+            return current;
+        }
+        return target;
+    }
     public static void main(String [] args)
     {
-        Path current = Path.of("/home/oualid");
+        Path current = Path.of("").toAbsolutePath();
+		Scanner sc = new Scanner(System.in);
 
-       current = current.resolve("Document");
-       current = current.resolve("obbad/../../HELLO");
-       current = current.normalize();
-       System.out.println(current);
-       System.out.println(current.getParent());
-       System.out.println(current.normalize());
-       System.out.println(current.getFileName());
-       System.out.println(current.isAbsolute());
+		while (true) {
+			System.out.println(current);
+			String arg = sc.nextLine();
+			if (arg.equals("42"))
+				return ;
+			current = changeDir(current, arg);
+		}
     }
 }
+// /home/
