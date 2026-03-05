c774709acfd3d80b0c72ef7f88da68c9574da527
Oualid Obbad <oobbad@c1r5p14.1337.ma>
2026-03-05 17:20:11 +0000

    finished ex02

--- Module03/ex02/Main.java
+++ Module03/ex02/Main.java
@@ -1,26 +1,48 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
-			Integer sizeArr;
-			Integer threadsCount;
-
 			if (args.length != 2)
 				throw new IllegalArgumentException("you must enter 2 Args => [ --arraySize=NB] [--threadsCount=NB]");
+
+			Integer sizeArr;
+			Integer threadsCount;
+			Integer[] arr;
+			Random random;
+
 			sizeArr = Integer.parseInt(args[0].trim().split("=")[1]);
-			threadsCount =  Integer.parseInt(args[1].trim().split("=")[1]);
+			threadsCount = Integer.parseInt(args[1].trim().split("=")[1]);
+			if (threadsCount <= 0 || threadsCount > sizeArr)
+				throw new IllegalArgumentException(
+						"The number of threads must be greater than one and not greater than size of array.");
+			if (sizeArr <= 0 || sizeArr >= 2_000_000)
+				throw new IllegalArgumentException("The size of Array must be greater than one and not greater than 2000000.");
+			random = new Random();
+			arr = new Integer[sizeArr];
+			for (int i = 0; i < sizeArr; i++){
+				arr[i] = random.nextInt(2000);
+			}
+			int sum = 0;
+			for (int i = 0; i < sizeArr; i++) {
+				sum += arr[i];
+			}
+			System.out.println("Sum: " + sum);
+			Thread threads[] = new RealMultithreading[threadsCount];
 
-			RealMultithreading.setThreadsCount(threadsCount);
-			System.out.println(RealMultithreading.getThreadsCount());
-		}
-		catch (ArrayIndexOutOfBoundsException e){
+			for (int i = 0; i < threadsCount; i++)
+				threads[i] = new RealMultithreading(i,threadsCount, arr);
+			for (int i = 0; i < threadsCount; i++)
+				threads[i].start();
+			for (int i = 0; i < threadsCount; i++)
+				threads[i].join();
+			System.out.println("Sum by threads: " + RealMultithreading.getGlobalSum());
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.err.println("Error: check args");
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			System.out.println("Error: check number of each arg");
-
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 	}
--- Module03/ex02/RealMultithreading.java
+++ Module03/ex02/RealMultithreading.java
@@ -1,22 +1,115 @@
+// ==================optimazed and simple code ===============
 public class RealMultithreading extends Thread {
-	private Integer [] arr;
+	
 	private static Integer threadsCount;
+	private static int globalSum = 0;
+	private Integer [] arr;
+	private int sizeOfEachOne;
+	private int myIndex;
+	private int start;
+	private int end;
+	private static Object lockSum = new Object();
 
-
-	public RealMultithreading(Integer size, Integer NbThreads){
-		if (NbThreads <= 0)
-			throw new IllegalArgumentException("The number of threads must be greater than one.");
+	public RealMultithreading(int myIndex, Integer NbThreads, Integer arr[]){
+		this.arr = arr;
 		threadsCount = NbThreads;
+		sizeOfEachOne = arr.length/NbThreads;
+		this.myIndex = myIndex;
 	}
+
 	@Override
 	public void run(){
-		
+		start = myIndex * sizeOfEachOne;
+		end = (myIndex == threadsCount -1) ? arr.length : start + sizeOfEachOne;
+		int sum = 0;
+		for (int i = start; i < end; i++){
+			sum += arr[i];
+		}
+		synchronized(lockSum){
+			globalSum +=sum;
+		}
+
+		System.out.println(getName() + ": from " + start + " to " + (end - 1) + " sum is " + sum);
+	}
+	public static int getGlobalSum() {
+		return globalSum;
 	}
 
-	static void setThreadsCount(Integer nbCount){
-		threadsCount = nbCount;
-	}
-	static Integer getThreadsCount(){
-		return threadsCount;
-	}
 }
+
+
+
+
+// ================== my fist Code ====================
+
+// public class RealMultithreading extends Thread {
+	
+// 	private static Integer threadsCount;
+// 	private static int indexOrder = 0;
+// 	private static int newStart = 0;
+// 	private static int shunkVar = 0;
+// 	private static int globalSum = 0;
+// 	private Integer [] arr;
+// 	private Integer size;
+// 	private int sizeOfEachOne;
+// 	private int myIndex;
+// 	private int start;
+// 	private int end;
+// 	private static Object lock = new Object();
+
+
+// 	public RealMultithreading(int myIndex, Integer size, Integer NbThreads, Integer arr[]){
+// 		this.size = size;
+// 		this.arr = arr;
+// 		threadsCount = NbThreads;
+// 		sizeOfEachOne = size/NbThreads;
+// 		this.myIndex = myIndex;
+// 	}
+// 	private void shunkArr(){
+// 		synchronized(lock){
+// 			while (indexOrder != myIndex) {
+// 				try {
+// 					lock.wait();
+// 				} catch (Exception e) {
+// 					Thread.currentThread().interrupt();
+// 					return ;
+// 				}
+// 			}
+// 			start = newStart;
+// 			if (indexOrder == threadsCount - 1){
+// 				end = size;
+// 				indexOrder = -1;
+// 			}
+// 			else{
+// 				end = shunkVar + sizeOfEachOne;
+// 			}
+// 			newStart = shunkVar + sizeOfEachOne;
+// 			shunkVar += sizeOfEachOne ;
+// 			indexOrder++;
+// 			lock.notifyAll();
+// 		}
+// 	}
+
+// 	public static void resetSharedState() {
+// 		indexOrder = 0;
+// 		newStart = 0;
+// 		shunkVar = 0;
+// 		globalSum = 0;
+// 	}
+// 	@Override
+// 	public void run(){
+// 		shunkArr();
+// 		int sum = 0;
+// 		for (int i = start; i < end; i++){
+// 			sum += arr[i];
+// 		}
+// 		synchronized(lock){
+// 			globalSum +=sum;
+// 		}
+// 		System.out.println(getName() + ": from " + start + " to " + (end - 1) + " sum is " + sum);
+// 	}
+// 	public static int getGlobalSum() {
+// 		return globalSum;
+// 	}
+
+// }
